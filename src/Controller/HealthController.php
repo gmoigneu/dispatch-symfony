8abd6561d046e7c03b46790a9c5c04dbecb3b511
Obvious Agent <obvious-agent@upsun.com>
2026-05-07 02:15:02 +0000

    obvious: implement plan for #14

--- src/Controller/HealthController.php
+++ src/Controller/HealthController.php
@@ -9,6 +9,22 @@ use Symfony\Component\Routing\Attribute\Route;
 class HealthController extends AbstractController
 {
     #[Route('/healthz', name: 'healthz', methods: ['GET'])]
+    public function index(): JsonResponse
+    {
+        return new JsonResponse(['status' => 'ok']);
+    }
+}
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HealthController extends AbstractController
+{
+    #[Route('/healthz', name: 'healthz', methods: ['GET'])]
     public function healthz(): JsonResponse
     {
         return new JsonResponse(['status' => 'ok']);
